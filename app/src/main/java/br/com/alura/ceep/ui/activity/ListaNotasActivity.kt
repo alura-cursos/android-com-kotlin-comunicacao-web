b61ac51601cf08e4a6a8d91d2ea16311ca3aee1d
Alex Felipe <alexfelipevieira@gmail.com>
2022-02-09 08:55:07 -0300

    2.1

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
@@ -13,6 +13,7 @@ import br.com.alura.ceep.databinding.ActivityListaNotasBinding
 import br.com.alura.ceep.extensions.vaiPara
 import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter
 import kotlinx.coroutines.launch
+import retrofit2.Retrofit
 
 class ListaNotasActivity : AppCompatActivity() {
 
@@ -36,6 +37,7 @@ class ListaNotasActivity : AppCompatActivity() {
                 buscaNotas()
             }
         }
+//        Retrofit
     }
 
     private fun configuraFab() {
