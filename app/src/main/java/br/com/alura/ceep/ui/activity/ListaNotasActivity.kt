919d10215a71bcafc54489786a5545be56b5a0cb
Alex Felipe <alexfelipevieira@gmail.com>
2022-02-09 10:36:13 -0300

    2.3

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
@@ -15,6 +15,7 @@ import br.com.alura.ceep.extensions.vaiPara
 import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter
 import br.com.alura.ceep.webclient.RetrofitInicializador
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
@@ -42,10 +43,12 @@ class ListaNotasActivity : AppCompatActivity() {
                 buscaNotas()
             }
         }
-        val call: Call<List<Nota>> = RetrofitInicializador().notaService.buscaTodas()
-        val resposta: Response<List<Nota>> = call.execute()
-        resposta.body()?.let { notas ->
-            Log.i("ListaNotas", "onCreate: $notas")
+        lifecycleScope.launch(IO) {
+            val call: Call<List<Nota>> = RetrofitInicializador().notaService.buscaTodas()
+            val resposta: Response<List<Nota>> = call.execute()
+            resposta.body()?.let { notas ->
+                Log.i("ListaNotas", "onCreate: $notas")
+            }
         }
     }
 
--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -2,11 +2,13 @@ package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.services.NotaService
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RetrofitInicializador {
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("http://localhost:8080/")
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     val notaService = retrofit.create(NotaService::class.java)
